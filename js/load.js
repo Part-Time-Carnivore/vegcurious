cb1f9bcb946b88dc80ee1aa95085757b6440ed09
Pete Davis <petedavis@Carys-Mulas-MacBook.local>
2017-02-27 22:23:50 +0000

    #1 Update localstorage and load from it

--- js/load.js
+++ js/load.js
@@ -1,56 +1,17 @@
 $(document).ready(function () {
-  var day = 24*60*60*1000;
-  var today = new Date();
 
-  // set previous date function
-  function getPrevDate(currentDate) {
-    var prevDate = $('select:first-of-type').attr('id');
-    // if no days added yet set prev to current
-    if (prevDate == null) prevDate = currentDate;
-    var prevDateFull = new Date(prevDate);
-    return prevDateFull;
+  // check localstorage to set vegDays
+  if (window.localStorage && localStorage.vegDays) {
+    vegDays = JSON.parse(localStorage['vegDays']);
   }
 
-  // load days function
-  function loadDays(date, count) {
-    $('main')
-      .prepend('<label for="' + date + '"><time>' + date + '</time> <select id="' + date + '" multiple="multiple"><option value="" disabled selected style="display: none;">Add veg</option></select> <i>' + count + '</i></label>');
-  }
-
-  // add missing days function
-  function missingDays(prevDateFull) {
-    var dateFull = new Date(prevDateFull.getTime() + day);
-    var date = dateFull.toISOString().split('T')[0];
-    var count = "0";
-    loadDays(date, count);
-    return dateFull;
-  }
-
-  // loop through days function
-  function loopDays() {
-    vegDays.forEach(function(d) {
-      var currentDate = d.date;
-      var currentDateFull = new Date(currentDate);
-      var prevDateFull = getPrevDate(currentDate);
-      // check if there's a gap between days
-      while (currentDateFull - prevDateFull > day) {
-        prevDateFull = missingDays(prevDateFull);
-      }
-      // generate current day
-      var date = currentDate;
-      var count = d.veg.length;
-      loadDays(date, count);
-    });
-  }
-
-  // check localstorage for vegDavs
-  if (window.localStorage) {
-    if (localStorage.vegDays) {
-      loopDays();
-    }
-    else {
-      loadDays(today.toISOString().split('T')[0], 0);
-    }
+  // if there is data in vegDays
+  if (vegDays.length) {
+    // load all days
+    loopDays();
+  } else {
+    // just load today
+    loadDays(today.toISOString().split('T')[0], 0);
   }
 
   // after loading days, add days up to today
--- js/update.js
+++ js/update.js
@@ -3,14 +3,41 @@
 
 $(document).ready(function () {
 
-  // update counts
+  // selection change event
   $("select").on("change", function (e) {
     // count items
     var count = $("+ .selectize-control .item", this).length;
 
     // update count html
-    $("+ .selectize-control + i", this)
-      .html(count);
+    $("+ .selectize-control + i", this).html(count);
+
+    // generate vegDays
+    vegDays = []
+    $('select').each(function() {
+      // add values to thisVeg array
+      var thisVeg = []
+      $('option:selected', this).each(function() {
+        var veg = parseInt(this.value)
+        thisVeg.push(veg);
+      });
+
+      //only get date and add to vegDays if there is thisVeg contains values
+      if (thisVeg.length != 0) {
+        // get date
+        var thisDate = $(this).attr('id');
+        // day object
+        var vegDay = {};
+        vegDay.date = thisDate;
+        vegDay.veg = thisVeg;
+        vegDays.unshift(vegDay);
+      }
+    });
+
+    // update localStorage
+    if (window.localStorage) {
+      localStorage.setItem('vegDays', JSON.stringify(vegDays));
+    }
+
   })
 
 });
