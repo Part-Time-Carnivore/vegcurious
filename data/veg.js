2aaa37df525fe96c4d619543d5e61baa73d0dfa4
Pete Davis <petedavis@Carys-Mulas-MacBook.local>
2017-02-28 20:42:08 +0000

    Added vegetables

--- data/veg.js
+++ data/veg.js
@@ -1,7 +1,14 @@
 var veg = [
-    { id: 1, name: 'Artichoke' }, 
+    { id: 1, name: 'Broccoli' }, 
     { id: 2, name: 'Cabbage' }, 
-    { id: 3, name: 'Sprouts' }, 
+    { id: 3, name: 'Kale' }, 
     { id: 4, name: 'Onion' }, 
-    { id: 5, name: 'Peas' }
+    { id: 5, name: 'Peas' }, 
+    { id: 6, name: 'Carrot' }, 
+    { id: 7, name: 'Garlic' }, 
+    { id: 8, name: 'Mushroom' }, 
+    { id: 9, name: 'Tomato' }, 
+    { id: 10, name: 'Sweet Potato' }, 
+    { id: 11, name: 'Cauliflower' },
+    { id: 12, name: 'Lettuce' }
 ];
