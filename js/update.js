6e9fb2344a1168f4cc3a1abe32d5021ae251d339
Pete Davis <petedavis@Carys-Mulas-MacBook.local>
2017-03-01 00:12:10 +0000

    #5 update average

--- js/update.js
+++ js/update.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
 
   // selection change event
   $("select").on("change", function (e) {
-    // count items
-    var count = $("+ .selectize-control .item", this).length;
-
-    // update count html
-    $("+ .selectize-control + i", this).html(count);
 
     // generate vegDays
-    vegDays = []
+    vegDays = [];
+    var totalVeg = 0;
+    var totalDays = 0;
+    var average = 0;
     $('select').each(function() {
       // add values to thisVeg array
       var thisVeg = []
@@ -17,6 +15,10 @@ $(document).ready(function () {
         var veg = parseInt(this.value)
         thisVeg.push(veg);
       });
+
+      // running totals for average
+      totalDays = totalDays + 1;
+      totalVeg = totalVeg + thisVeg.length;
 
       //only get date and add to vegDays if there is thisVeg contains values
       if (thisVeg.length != 0) {
@@ -30,10 +32,20 @@ $(document).ready(function () {
       }
     });
 
+    // average veg per day
+    average = (totalVeg / totalDays).toPrecision(3);
+    $('#average').html(average);
+
     // update localStorage
     if (window.localStorage) {
       localStorage.setItem('vegDays', JSON.stringify(vegDays));
     }
+
+    // count items
+    var count = $("+ .selectize-control .item", this).length;
+
+    // update count html
+    $("+ .selectize-control + i", this).html(count);
 
   })
 
